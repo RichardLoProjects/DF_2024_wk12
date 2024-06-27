5abfa358e7d4c98c0b3be16ad011c8289059d10a
Richard Lo <90041190+RichardLoProjects@users.noreply.github.com>
2024-06-27 12:28:13 +0100

    20240627_pipeline

--- pipeline.py
+++ pipeline.py
@@ -9,6 +9,7 @@ import psycopg2 as psql # type: ignore
 import warnings
 
 
+
 '''Classes'''
 class EnvSecrets:
     def __init__(self) -> None:
@@ -30,17 +31,25 @@ class EnvSecrets:
 class DatabaseConnection:
     def __init__(self, secret:EnvSecrets) -> None:
         '''Open database connection.'''
-        self.connection = psql.connect(
-            database = secret.db_name
-            , host = secret.db_host
-            , port = secret.db_port
-            , user = secret.sql_user
-            , password = secret.sql_pass
-        )
-        self.cursor = self.connection.cursor()
+        try:
+            self.connection = psql.connect(
+                database = secret.db_name
+                , host = secret.db_host
+                , port = secret.db_port
+                , user = secret.sql_user
+                , password = secret.sql_pass
+            )
+            self.cursor = self.connection.cursor()
+            self.valid = True
+        except:
+            '''Possible expansion for email sending, notifying database failure or invalid credentials.'''
+            self.valid = False
     def close(self) -> None:
         '''Close database connection.'''
-        self.connection.close()
+        try:
+            self.connection.close()
+        except:
+            pass
 
 class DataPipeline:
     def __init__(self, secret) -> None:
@@ -49,8 +58,16 @@ class DataPipeline:
     def extract(self, polymorphism) -> None:
         '''Get request from API. Create dataframe. Populate dataframe'''
         self._df = polymorphism(self._secret)
-    def transform(self) -> None:
-        '''Data cleaning: fix dtypes. Feature engineering.'''
+    def transform(self, polymorphism) -> None:
+        '''Data cleaning and feature engineering.'''
+        self._dtype_cleaning()
+        self._df = polymorphism(self._df)
+    def load(self, database, polymorphism) -> None:
+        '''Create table if not exist. Filter and commit delta of new data.'''
+        polymorphism(self._secret, self._df, database)
+        database.connection.commit()
+    def _dtype_cleaning(self) -> None:
+        '''Correcting data types'''
         for column in self._df.columns:
             if self._df[column].dtype == 'object':
                 try:
@@ -67,10 +84,7 @@ class DataPipeline:
                     self._df[column] = pd.to_datetime(self._df[column], unit='s')
                 except:
                     pass
-    def load(self, database, polymorphism) -> None:
-        '''Create table if not exist. Filter and commit delta of new data.'''
-        polymorphism(self._secret, self._df, database)
-        database.connection.commit()
+
 
 
 '''Polymorphism'''
@@ -115,6 +129,10 @@ def pipeline1_dynamic_extract(secret:EnvSecrets) -> pd.DataFrame:
                 _df_row[k] = int(v)
             except:
                 pass
+        if _df_row.get('avg_high_price') and _df_row.get('avg_low_price'):
+            _high = _df_row['avg_high_price']
+            _low = _df_row['avg_low_price']
+            _df_row['avg_high_price'], _df_row['avg_low_price'] = max(_high, _low), min(_high, _low)
         temp_df = pd.DataFrame.from_dict([_df_row], orient='columns')
         _df = pd.concat([_df, temp_df])
     _df.set_index('item_id', inplace=True)
@@ -122,10 +140,32 @@ def pipeline1_dynamic_extract(secret:EnvSecrets) -> pd.DataFrame:
     _df.reset_index(inplace=True)
     return _df
 
-def pipeline1_static_load(secret:EnvSecrets, df, database):
+def pipeline1_static_transform(_df) -> pd.DataFrame:
+    return _df
+
+def pipeline1_dynamic_transform(_df) -> pd.DataFrame:
+    pa = _df['avg_high_price']
+    pb = _df['avg_low_price']
+    va = _df['high_price_volume']
+    vb = _df['low_price_volume']
+    _df['total_volume'] = va + vb
+    _df['avg_mid_price'] = (pa + pb)//2
+    _df['avg_micro_price'] = np.where(
+        va + vb != 0
+        , ((pa*vb)//(va+vb)) + ((pb*va)//(va+vb))
+        , None
+    )
+    _df['wide_spread'] = np.where(
+        (pa.notna() & pb.notna()) & (pa-np.minimum(5_000_000,pa//100) > pb+1_000_000)
+        , True
+        , False
+    )
+    return _df
+
+def pipeline1_static_load(secret:EnvSecrets, _df, database):
     primary_key = 'item_id'
     column_definitions = []
-    for column_name, dtype in zip(df.columns, df.dtypes):
+    for column_name, dtype in zip(_df.columns, _df.dtypes):
         if pd.api.types.is_integer_dtype(dtype):
             sql_type = 'int'
         elif pd.api.types.is_float_dtype(dtype):
@@ -142,11 +182,11 @@ def pipeline1_static_load(secret:EnvSecrets, df, database):
             column_definitions.append(f'{column_name} {sql_type}')
     create_table_sql = f'CREATE TABLE IF NOT EXISTS {secret.tname_static} ({", ".join(column_definitions)});'
     database.cursor.execute(create_table_sql)
-    df = df.replace({np.NaN: None})
+    _df = _df.replace({np.NaN: None})
     query = f'SELECT {primary_key} FROM {secret.tname_static}'
     existing_data = pd.read_sql(query, database.connection)
     existing_set = set(existing_data[primary_key])
-    non_duplicate_data = df[~df[primary_key].isin(existing_set)]
+    non_duplicate_data = _df[~_df[primary_key].isin(existing_set)]
     if not non_duplicate_data.empty:
         tuples = [tuple(x) for x in non_duplicate_data.to_numpy()]
         columns = ','.join(non_duplicate_data.columns)
@@ -154,29 +194,33 @@ def pipeline1_static_load(secret:EnvSecrets, df, database):
         insert_query = f'INSERT INTO {secret.tname_static} ({columns}) VALUES ({values})'
         database.cursor.executemany(insert_query, tuples)
 
-def pipeline1_dynamic_load(secret:EnvSecrets, df, database):
+def pipeline1_dynamic_load(secret:EnvSecrets, _df, database):
     create_table_sql = f'''
 CREATE TABLE IF NOT EXISTS {secret.tname_dynamic} (
-    item_id int,
-    price_timestamp timestamp,
-    avg_high_price int,
-    avg_low_price int,
-    high_price_volume int,
-    low_price_volume int,
-    PRIMARY KEY (item_id, price_timestamp),
-    FOREIGN KEY (item_id) REFERENCES {secret.tname_static}(item_id)
+    item_id int
+    , price_timestamp timestamp
+    , avg_high_price int
+    , avg_low_price int
+    , high_price_volume int
+    , low_price_volume int
+    , total_volume int
+    , avg_mid_price float
+    , avg_micro_price float
+    , wide_spread boolean
+    , PRIMARY KEY (item_id, price_timestamp)
+    , FOREIGN KEY (item_id) REFERENCES {secret.tname_static}(item_id)
 );
 '''
     database.cursor.execute(create_table_sql)
-    df = df.replace({np.NaN: None})
+    _df = _df.replace({np.NaN: None})
     composite_key_columns = ['item_id', 'price_timestamp']
     query = f'SELECT {", ".join(composite_key_columns)} FROM {secret.tname_dynamic}'
     existing_data = pd.read_sql(query, database.connection)
     existing_set = set([tuple(x) for x in existing_data.to_numpy()])
-    new_data_set = set([tuple(x) for x in df[composite_key_columns].to_numpy()])
-    non_duplicate_keys = new_data_set - existing_set
-    non_duplicate_data = df[
-        df.apply(lambda row: tuple(row[composite_key_columns]), axis=1).isin(non_duplicate_keys)
+    incoming_data_set = set([tuple(x) for x in _df[composite_key_columns].to_numpy()])
+    non_duplicate_keys = incoming_data_set - existing_set
+    non_duplicate_data = _df[
+        _df.apply(lambda row: tuple(row[composite_key_columns]), axis=1).isin(non_duplicate_keys)
     ]
     if not non_duplicate_data.empty:
         tuples = [tuple(x) for x in non_duplicate_data.to_numpy()]
@@ -184,6 +228,10 @@ CREATE TABLE IF NOT EXISTS {secret.tname_dynamic} (
         values = ','.join(['%s'] * len(non_duplicate_data.columns))
         insert_query = f'INSERT INTO {secret.tname_dynamic} ({columns}) VALUES ({values})'
         database.cursor.executemany(insert_query, tuples)
+
+def send_email(error_message):
+    pass
+
 
 
 '''Main'''
@@ -195,18 +243,25 @@ def main() -> None:
         sql = f'SELECT EXISTS (SELECT * FROM information_schema.tables WHERE table_name = \'{my_secrets.api1_static}\');'
         my_database.cursor.execute(sql)
         table_exist = my_database.cursor.fetchall()[0][0]
-        if not table_exist:
+        if  not table_exist:
             pipeline1_static = DataPipeline(my_secrets)
             pipeline1_static.extract(pipeline1_static_extract)
-            pipeline1_static.transform()
+            pipeline1_static.transform(pipeline1_static_transform)
             pipeline1_static.load(my_database, pipeline1_static_load)
         pipeline1_dynamic = DataPipeline(my_secrets)
         pipeline1_dynamic.extract(pipeline1_dynamic_extract)
-        pipeline1_dynamic.transform()
+        pipeline1_dynamic.transform(pipeline1_dynamic_transform)
         pipeline1_dynamic.load(my_database, pipeline1_dynamic_load)
     except Exception as e:
-        print(e)
-        pass
+        send_email(e)
+        '''Emergency csv dump'''
+        try:
+            pipeline1_dynamic = DataPipeline(my_secrets)
+            pipeline1_dynamic.extract(pipeline1_dynamic_extract)
+            pipeline1_dynamic.transform(pipeline1_dynamic_transform)
+            pipeline1_dynamic._df.to_csv(f'osrs_emergency_csv_dump/{int(time.time())}.csv')
+        except:
+            pass
     finally:
         my_database.close()
 
